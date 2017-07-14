caaf73ef69b2cbde0e74cc32a7618b82aff29ef3
Ajitesh Rai <ajiteshr7@gmail.com>
2017-07-14 23:12:59 +0530

    setting up to host it on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn art_web:app
--- art_web/__init__.py
+++ art_web/__init__.py
@@ -1,9 +1,10 @@
+from os import environ
 from flask import Flask
 
 app = Flask(__name__)
 
 app.secret_key = 'mnbvcxz92+iobugvy554576*()%~`5rsaxfi8716jhajh$^*'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@127.0.0.1:3306/art_gallery'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'mysql://root:1234@127.0.0.1:3306/art_gallery')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # app.config['BASIC_AUTH_USERNAME'] = 'admin'
 # app.config['BASIC_AUTH_PASSWORD'] = 'admin'
